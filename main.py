1042d1b2832d79b631e91f77c1346a7efdd3dff8
marinacruzjordao <marinacruzjordao@gmail.com>
2021-02-02 18:04:57 +0000

    initiate GUI

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 # environment: War between two nations, side North, sigh South
 #
+from PySimpleGUI import PySimpleGUI as sg
 
 class Aventure:
     def __init__(self):
@@ -14,26 +15,46 @@ class Aventure:
         self.end3 = 'You are a hero in the shadows!'
         self.end4 = 'You are not able to fight in this battle!'
 
+    def data(self):
+        self.event, self.value = self.w1.read()
 
     def start(self):
-        a1=input(self.q1)
+        # create layout
+        sg.theme('Reddit')
 
-        if a1=='North' or a1=='north':
-            a2=input(self.q2)
-            if a2 == 'Sword' or a2 == 'sword':
-                print(self.end1)
-            elif a2 == 'Shield' or a2 == 'shield':
-                print(self.end2)
-            else:
-                print('This option is not valid.')
-        elif a1=='South' or a1=='south':
-            a3=input(self.q3)
-            if a2 == 'Combat' or a2 == 'combat':
-                print(self.end3)
-            elif a2 == 'Tactic' or a2 == 'tactic':
-                print(self.end4)
-            else:
-                print('This option is not valid.')
+        layout = [
+            [sg.Text('Question:'),sg.Output(key='question')],  # line1
+            [sg.Text('Your answer:'),sg.Input(key='answer')],  # line 2
+            [sg.Button('Start'),sg.Button('Play')]  # Line 3
+        ]
+
+        # create a window in GUI
+        self.w1 = sg.Window('Adventure Game').layout(layout)
+
+        self.data()
+        if self.event=='Start':
+            print(self.q1)
+            self.data()
+            if self.event=='Play':
+                self.data()
+                if self.value=='North' or self.value=='north':
+                    self.data()
+                    if self.event == 'Play':
+                        self.data()
+                        if self.value == 'Sword' or self.value == 'sword':
+                            print(self.end1)
+                        elif self.value == 'Shield' or self.value == 'shield':
+                            print(self.end2)
+                        else:
+                            print('This option is not valid.')
+                elif self.value=='South' or self.value=='south':
+                    self.data()
+                    if self.value == 'Combat' or self.value == 'combat':
+                        print(self.end3)
+                    elif self.value== 'Tactic' or self.value== 'tactic':
+                        print(self.end4)
+                    else:
+                        print('This option is not valid.')
 
 a=Aventure()
 a.start()
